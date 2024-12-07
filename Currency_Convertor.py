dbf3d646e081ff3ff014ef0291f6d36716fd2f56
Ashish-Thapa16 <ashishthapa160120@gmail.com>
2024-12-07 20:27:19 +0530

    AddCurrencyConvertor

--- Currency_Convertor.py
+++ Currency_Convertor.py
@@ -3,7 +3,7 @@ import requests
 
 currencies = ["USD", "INR", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY", "NZD", "SEK", "MXN", "SGD", "HKD", "NOK", "KRW", "TRY", "RUB", "ZAR", "BRL", "MYR", "THB", "PHP", "IDR", "AED", "SAR", "EGP", "ILS", "PLN", "DKK", "HUF", "CZK", "CLP", "COP", "PEN", "MAD", "LKR", "VND", "KWD", "BHD", "OMR", "QAR", "KZT", "RSD", "HRK", "RON", "BGN", "MKD", "LTL", "LVL", "EEK", "ISK", "ALL", "MDL", "MNT", "TWD", "KRW", "BAM", "GEL", "KGS", "UZS"]
 
-def get_exchangeRate(user_base_currency, user_target_currency) :
+def exchangeRate(user_base_currency, user_target_currency) :
     url = f"https://api.exchangerate-api.com/v4/latest/{user_base_currency}"
     response = requests.get(url)
 
@@ -14,29 +14,26 @@ def get_exchangeRate(user_base_currency, user_target_currency) :
         if user_target_currency in rates :
             return rates[user_target_currency]
         else :
-            print(f"Error: {user_target_currency} not found in rates")
-
+            return f"Error: {user_target_currency} not found."
+        
     else :
         print(f"Error: {response.status_code}")
-    return None
-
+        return None
+    
+def currencyConvertor(user_base_currency, user_amount, user_target_currency) :
+    rate = exchangeRate(user_base_currency, user_target_currency)
+    if rate :
+        return user_amount * rate
+    else :
+        return "Conversion Failed!"
 
 user_base_currency = input(f"Enter your currency from {currencies}: ").upper()
-user_amount = int(input("Enter your amount: "))
+user_amount = float(input("Enter your amount: "))
 user_target_currency = input("Enter the currency you want your amount to be converted: ").upper()
 
-
-def convertor(user_amount, user_base_currency, user_target_currency) :
-    rate = get_exchangeRate(user_base_currency, user_target_currency)
-    if rate:
-        return rate
-    else:
-        return None
-
-result = convertor(user_amount, user_base_currency, user_target_currency)
+result = currencyConvertor(user_base_currency, user_amount, user_target_currency)
 print(result)
 
 
     
-
 
